32584a1393436d5b897a61bd54259fd921fd87e6
Mahsa Piran <piran_mahsa@yahoo.com>
2024-04-20 15:14:53 +0330

    Route Changed

--- MyResume/routes/web.php
+++ MyResume/routes/web.php
@@ -2,7 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);

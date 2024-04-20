59dbfd7fa368220e05a994a5fd2c57783328d56d
Mahsa Piran <piran_mahsa@yahoo.com>
2024-04-20 13:12:46 +0330

    Get Data from DB
    create HomeController and get all data from DB

---
+++ MyResume/app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Award;
+use App\Models\EducationLevel;
+use App\Models\Experience;
+use App\Models\ProfileData;
+use App\Models\Skill;
+use App\Models\Work;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $profile_data = ProfileData::first();
+
+        $awards=Award::all()->sortDesc();
+
+        $experiences=Experience::all()->sortDesc();
+
+        $education_levels=EducationLevel::all()->sortDesc();
+
+        $skills=Skill::all()->sortDesc();
+
+        $works=Work::all()->sortDesc();
+
+        return view('index',compact(
+            'profile_data',
+            'awards',
+            'experiences',
+            'education_levels',
+            'skills',
+            'works'
+        ));
+    }
+}
--- MyResume/routes/web.php
+++ MyResume/routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
